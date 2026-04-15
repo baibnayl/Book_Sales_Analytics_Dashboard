45f5bb86bd3976ba1916c36597cdcfff30c5ed8a
baibnayl <70564130+baibnayl@users.noreply.github.com>
2026-04-15 15:31:41 +0600

    Add files via upload

---
+++ dashboard_data.py
@@ -0,0 +1,44 @@
+PAGE_ONE_DATA = {
+    "unique_users": 3115,
+    "unique_author_sets": 325,
+    "most_popular_authors": "Arlinda Huel",
+    "best_buyer_ids": [45062, 46955, 44850],
+    "top_5_days_data": [
+        {"Rank": 1, "Date": "2024-11-14", "Revenue": 3911.99},
+        {"Rank": 2, "Date": "2025-01-21", "Revenue": 3509.7},
+        {"Rank": 3, "Date": "2024-10-12", "Revenue": 3499.12},
+        {"Rank": 4, "Date": "2025-01-25", "Revenue": 3423.43},
+        {"Rank": 5, "Date": "2024-11-09", "Revenue": 3283.92},
+    ],
+    "daily_revenue_data": "daily_revenue_data_1.csv"
+}
+
+PAGE_TWO_DATA = {
+    "unique_users": 2663,
+    "unique_author_sets": 293,
+    "most_popular_authors": "Hershel Treutel, Miss Modesto Denesik, Sen. Trula Bosco",
+    "best_buyer_ids": [53583, 55058, 55420],
+    "top_5_days_data": [
+        {"Rank": 1, "Date": "2024-11-15", "Revenue": 3677.17},
+        {"Rank": 2, "Date": "2024-11-21", "Revenue": 3590.09},
+        {"Rank": 3, "Date": "2024-12-25", "Revenue": 3572.11},
+        {"Rank": 4, "Date": "2025-01-20", "Revenue": 3561.51},
+        {"Rank": 5, "Date": "2024-12-14", "Revenue": 3514.39},
+    ],
+    "daily_revenue_data": "daily_revenue_data_2.csv"
+}
+
+PAGE_THREE_DATA = {
+    "unique_users": 3290,
+    "unique_author_sets": 268,
+    "most_popular_authors": "Coy Streich, Keeley Hand, Lela Emard",
+    "best_buyer_ids": [49715, 50963],
+    "top_5_days_data": [
+        {"Rank": 1, "Date": "2024-11-16", "Revenue": 3688.2},
+        {"Rank": 2, "Date": "2024-11-12", "Revenue": 3411.99},
+        {"Rank": 3, "Date": "2025-01-31", "Revenue": 3341.99},
+        {"Rank": 4, "Date": "2024-10-26", "Revenue": 3127.92},
+        {"Rank": 5, "Date": "2024-12-28", "Revenue": 2933.7},
+    ],
+    "daily_revenue_data": "daily_revenue_data_3.csv"
+}
